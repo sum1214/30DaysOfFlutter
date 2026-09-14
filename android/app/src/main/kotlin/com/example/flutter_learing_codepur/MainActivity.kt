6a9fd18bd16da3791477948a484a274daf611b9a
shubham <shubham@visio-apps.com>
2026-09-14 15:11:55 +0530

    first commit

---
+++ android/app/src/main/kotlin/com/example/flutter_learing_codepur/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.flutter_learing_codepur
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
---
+++ lib/main.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(home: Center(child: Material(child: Text('Welcome to 30 days of flutter'))));
+  }
+}
